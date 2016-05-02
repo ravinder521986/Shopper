e184653e35929d3ddff24013c5119968fd50dc2b
Ravinder <ravinderk_1986@yahoo.com>
2016-05-02 12:08:02 +0530

    o
    k

---
+++ Design/mamtaViewController.h
@@ -0,0 +1,13 @@
+//
+//  mamtaViewController.h
+//  Project
+//
+//  Created by MAC on 02/05/16.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mamtaViewController : UIViewController
+
+@end
---
+++ Design/mamtaViewController.m
@@ -0,0 +1,37 @@
+//
+//  mamtaViewController.m
+//  Project
+//
+//  Created by MAC on 02/05/16.
+//  Copyright © 2016 MAC. All rights reserved.
+//
+
+#import "mamtaViewController.h"
+
+@interface mamtaViewController ()
+
+@end
+
+@implementation mamtaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
